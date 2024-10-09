b83906830adcbc7b13fad2fbd8f8759795b64501
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-10-09 21:09:51 +0600

    Add userinfo saving feature

--- client/src/pages/onboarding.jsx
+++ client/src/pages/onboarding.jsx
@@ -1,14 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { useStateProvider } from "@/context/StateContext";
 import Input from "@/components/common/Input";
 import Avatar from "@/components/common/Avatar";
+import { saveUserInfo } from "@/utils/apiRoutes";
+import { reducerCases } from "@/context/constants";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 function onboarding() {
+  const router = useRouter();
   const [state, dispatch] = useStateProvider();
   const [name, setName] = useState(state.userInfo.name || "");
   const [about, setAbout] = useState("");
   const [image, setImage] = useState("/default_avatar.png");
+
+  useEffect(() => {
+    if (!state.newUser && !state.userInfo.email) {
+      router.push("/login");
+    }
+    if (!state.newUser && state.userInfo.email) {
+      router.push("/");
+    }
+  }, []);
+
+  const saveUserInfoHandler = async () => {
+    if (validateInfo()) {
+      const email = state.userInfo.email;
+      console.log("the email is  ", email);
+      try {
+        const response = await axios.post(saveUserInfo, {
+          name,
+          email,
+          about,
+          image,
+        });
+        if (response.status === 201) {
+          dispatch({ type: reducerCases.SET_NEW_USER, newUser: false });
+          dispatch({
+            type: reducerCases.SET_USER_INFO,
+            userInfo: { name, email, about, proiflePic: image },
+          });
+          router.push("/");
+        }
+      } catch (error) {
+        console.error("error while saving user data ", error);
+      }
+    }
+  };
+  const validateInfo = () => { return true};
   return (
     <div className="bg-panel-header-background h-screen w-screen text-white flex justify-center items-center flex-col">
       <div className="flex justify-center items-center gap-2">
@@ -20,9 +60,17 @@ function onboarding() {
         <div className="flex flex-col justify-center items-center mt-5">
           <Input name="Name" state={name} setState={setName} label />
           <Input name="About" state={about} setState={setAbout} label />
+          <div className="flex justify-center items-center">
+            <button
+              className="flex justify-center items-center gap-2 bg-search-input-container-background p-3 rounded-md mt-5"
+              onClick={saveUserInfoHandler}
+            >
+              Create Profile
+            </button>
+          </div>
         </div>
         <div className="mt-10">
-          <Avatar type='xl' image={image} setImage={setImage}/>
+          <Avatar type="xl" image={image} setImage={setImage} />
         </div>
       </div>
     </div>
--- client/src/utils/ApiRoutes.js
+++ client/src/utils/ApiRoutes.js
@@ -3,3 +3,4 @@ export const Host = process.env.NEXT_PUBLIC_HOST;
 export const authRoute = `${Host}/api/auth`;
 
 export const checkUser = `${authRoute}/checkUser`;
+export const saveUserInfo = `${authRoute}/saveUserInfo`;
