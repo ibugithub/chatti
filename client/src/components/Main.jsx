63cba980b249a7203955ac0702c409e2ad95295c
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-10-12 06:35:32 +0600

    Design ChatHeder page

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import {ChatList} from "./Chatlist/ChatList";
 import { Empty } from "./Empty";
+import {Chat} from "./Chat/Chat";
 
 export const Main =() => {
   return(
     <>
       <div className="grid grid-cols-main h-screen  w-screen max-h-screen max-w-full overflow-hidden">
           <ChatList />
-          <Empty />
+          {/* <Empty /> */} 
+          <Chat />
       </div>
     </>
   )
